3e56ecb16b1a83ea7c3e5ec9ffbc20d2dea1f381
Marcus Ortyl <Mortyl@yvonnes-MacBook-Air.local>
2018-02-08 16:15:30 +0000

    javascript updates v0.3

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,35 @@
 	let nav = d.getElementById('js__nav');
 
 	d.getElementById('js__nav').addEventListener('click', ev => {
-		smoothScroll(ev);
+		if (ev.target.tagName === 'A') {
+			smoothScroll(ev);
+		}
 	});
 
 	function smoothScroll(event) {
-	    if (event.target.hash !== 'A') { 
-	        event.preventDefault()
-	        let hash = event.target.hash.replace("#", "")
-	        let link = document.getElementById(hash)
-	        const position = link.getBoundingClientRect().y 
-	        let top = 0
+        event.preventDefault()
+        let hash = event.target.hash.replace("#", "")
+        let link = document.getElementById(hash)
+        const position = link.getBoundingClientRect().y 
+        let top = 0
 
-	        let smooth = setInterval(() => {
-	            let leftover = position - top
-	            if (top === position) {
-	                clearInterval(smooth)
-	            }
+        let smooth = setInterval(() => {
+            let leftover = position - top
+            if (top === position) {
+                clearInterval(smooth)
+            }
 
-	            else if(position > top && leftover < 10) {
-	                top += leftover
-	                window.scrollTo(0, top)
-	            }
+            else if(position > top && leftover < 10) {
+                top += leftover
+                window.scrollTo(0, top)
+            }
 
-	            else if(position > (top - 10)) {
-	                top += 10
-	                window.scrollTo(0, top)
-	            }
+            else if(position > (top - 10)) {
+                top += 10
+                window.scrollTo(0, top)
+            }
 
-	        }, 3)
-	    }
+        }, 3)
 	}
 
 })(document)
